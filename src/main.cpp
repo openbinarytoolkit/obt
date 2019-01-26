e4b740119d6d98352231a9ec55dc68380091e4b4
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2019-01-26 15:50:07 +0100

    added binaryBuffer + file reader

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 
+#include <io.hpp>
+
 using namespace std;
 
 int main(int argc, char**argv)
 {
-	cout << "Hello World" << endl;
+	cout << "OpenBinaryToolkit" << endl;
+
+	if(argc > 1)
+	{
+		BinaryBuffer buffer = BinaryBuffer::new_from_file(argv[1]);
+		cout << " - ELF HEADER DEMO" << endl;
+		for(int x = 1; x < 4; x++)
+		{
+			cout << (char) buffer.bytes()->at(x);
+		}
+		cout << endl << "------ " << endl;
+		cout << "Size: " << buffer.bytes()->size() << " bytes" << endl;
+	}
+	else
+	{
+		cout << "Please specify filename" << endl;
+	}
+
 	return 0;
 }
