13a6a9b03931c23f43778d5bf364f6122b288f52
Thorsten6697 <thorsten6697@gmail.com>
2019-02-27 22:30:50 +0100

    Fixed reading of section names

--- src/elf.cpp
+++ src/elf.cpp
@@ -6,15 +6,21 @@ ELFFile64::ELFFile64(BinaryBuffer buffer)
 {
 	this->buffer = buffer;
 	memcpy(&elf_header, buffer.bytes()->data(), sizeof(Elf64_Ehdr));
+
+	size_t name_section_offset = elf_header.e_shoff + elf_header.e_shstrndx * elf_header.e_shentsize;
+	BinaryBuffer name_section_header_buffer = this->buffer.slice(name_section_offset, sizeof(Elf64_Shdr));
+	Elf64_Shdr name_section_header;
+	memcpy(&name_section_header, name_section_header_buffer.bytes()->data(), sizeof(Elf64_Shdr));
+
 	for(size_t i = 0; i < elf_header.e_shnum; i++)
 	{
-		BinaryBuffer section = this->buffer.slice(elf_header.e_shoff + i * elf_header.e_shentsize, elf_header.e_shentsize);
+		BinaryBuffer section_header_buffer = this->buffer.slice(elf_header.e_shoff + i * elf_header.e_shentsize, sizeof(Elf64_Shdr));
+		Elf64_Shdr section_header;
+		memcpy(&section_header, section_header_buffer.bytes()->data(), sizeof(Elf64_Shdr));
 
-		Elf64_Shdr sec;
-		memcpy(&sec, section.bytes()->data(), sizeof(Elf64_Shdr));
-		size_t secNameOffset = sec.sh_name + (elf_header.e_shstrndx * elf_header.e_shentsize) + elf_header.e_shoff;
-		cout << "Section Name at: " << secNameOffset  << endl;
-		cout << buffer.get_string_at_offset(secNameOffset) << endl;
+
+		cout << "Section Name at: " << (name_section_header.sh_offset + section_header.sh_name) << endl;
+		cout << buffer.get_string_at_offset(name_section_header.sh_offset + section_header.sh_name) << endl;
 	}
 }
 
